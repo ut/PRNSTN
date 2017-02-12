8e78b5bc57de6b6f5892fe9cfadb14f8d4e50430
ut <ulf.treger@googlemail.com>
2017-02-12 20:44:31 +0100

    some rspecs

--- spec/prnstn_spec.rb
+++ spec/prnstn_spec.rb
@@ -8,4 +8,19 @@ describe Prnstn do
   it 'does something useful' do
     expect(false).to eq(false)
   end
+
+  it "check default CLI options" do
+      options = {}
+      pnr = Prnstn::Main.new(options)
+      expect(pnr.options[:instant_print]).to be true
+  end
+
+  it "check value swap for instant_print option" do
+      options = {:onpush_print => true}
+      pnr = Prnstn::Main.new(options)
+      expect(pnr.options[:instant_print]).to be nil
+  end
+
+  ### TODO: test logger
+
 end
